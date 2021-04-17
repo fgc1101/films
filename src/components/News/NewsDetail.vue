3a412ae0ce95067de9fffc5290df6b6f112e775a
fanguochao <544942125@qq.com>
2021-04-17 17:06:58 +0800

    新闻详情页面

--- src/components/News/NewsDetail.vue
+++ src/components/News/NewsDetail.vue
@@ -1,15 +1,62 @@
 <template>
-    <div>
-        <h1>新闻详情组件</h1>
+    <div class="new_detail">
+        <nav-bar :title="title"></nav-bar>
+        <div class="news-title">
+            <p>{{detail_info.title}}</p>
+            <div>
+                <span>{{detail_info.click_num}}次点击</span>
+                <span>分类: 民生经济</span>
+                <span>添加时间: 2021年04月17日</span>
+            </div>
+        </div>
+        <div class="news-detail" v-html="detail_info.content">
+            <p>哈哈哈</p>
+        </div>
     </div>
 </template>
 
 <script>
   export default {
-    name: "NewsDetail"
+    name: "NewsDetail",
+    data(){
+      return {
+        title:'新闻详情',
+        detail_info: {},
+      }
+    },
+    created() {
+      console.log(this.$route.params)
+      let { id } = this.$route.params
+      this.$axios.get('/api/index/newsDetail/'+id).then(res=>{
+        console.log(res);
+        this.detail_info = res.data.data;
+      })
+
+    }
   }
 </script>
 
 <style lang="less" scoped>
+    .news-title p {
+        color: #0a87f8;
+        font-size: 20px;
+        font-weight: bold;
+    }
+
+    .news-title span {
+        margin-right: 30px;
+    }
+
+    .news-title {
+        margin-top: 5px;
+        border-bottom: 1px solid rgba(0, 0, 0, 0.2);
+    }
+
+
+    /*主体文章的左右距离*/
+
+    .news-content {
+        padding: 10px 5px;
+    }
 
 </style>
--- src/main.js
+++ src/main.js
@@ -4,6 +4,7 @@ import router from './router'
 import MintUI from 'mint-ui'
 import 'mint-ui/lib/style.css'
 import './static/css/global.css'
+import './assets/common/global.less'
 // import FgcInstaller from './plugins/FgcInstaller'
 
 import axios from 'axios'
